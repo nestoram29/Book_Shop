d24b0c80b7841e08662edf641e1998b0da734d0f
Nestor Molina <60123547+nestoram29@users.noreply.github.com>
2022-12-21 14:06:57 -0600

    Removed sensitive material

--- BookCart/settings.py
+++ BookCart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4rj_i+z=^i+zj2b2%-a+52=n$5kr997=rt#loqn4_l!_xqfp*'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
